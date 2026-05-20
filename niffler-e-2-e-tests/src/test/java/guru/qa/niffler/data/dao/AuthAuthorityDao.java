63adbb7a546c22473da2fe68fc0f7aa6f6417c6e
sasoriq <vikotoed@gmail.com>
2026-05-21 02:12:46 +0700

    add jdbc implementations of user and authority dao

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/AuthAuthorityDao.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/AuthAuthorityDao.java
@@ -5,7 +5,6 @@ import guru.qa.niffler.data.entity.AuthAuthorityEntity;
 import java.util.List;
 
 public interface AuthAuthorityDao {
-    List<AuthAuthorityEntity> getAuthoritiesByUserId(List<AuthAuthorityEntity> authorities);
 
-    void deleteAuthorities(List<AuthAuthorityEntity> authorities);
+    List<AuthAuthorityEntity> create(List<AuthAuthorityEntity> authorities);
 }
