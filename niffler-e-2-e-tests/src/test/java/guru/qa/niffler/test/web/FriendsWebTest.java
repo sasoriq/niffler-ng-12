bcbf0e8fc03ab44e9c9b11bfbd3722824f8a06dc
sasoriq <vikotoed@gmail.com>
2026-05-21 20:50:43 +0700

    add RandomDataUtils

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
@@ -3,7 +3,6 @@ package guru.qa.niffler.test.web;
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
-import guru.qa.niffler.jupiter.extension.BrowserExtension;
 import guru.qa.niffler.jupiter.extension.UsersQueueExtension;
 import guru.qa.niffler.page.LoginPage;
 import org.junit.jupiter.api.Test;
