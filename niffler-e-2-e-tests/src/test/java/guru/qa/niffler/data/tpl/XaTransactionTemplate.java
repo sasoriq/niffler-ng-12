aa7de95aadc7a7bdb0e63d1c2d033a939489b7cc
sasoriq <vikotoed@gmail.com>
2026-05-27 01:50:19 +0700

    fix unused imports

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/tpl/XaTransactionTemplate.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/tpl/XaTransactionTemplate.java
@@ -1,11 +1,9 @@
 package guru.qa.niffler.data.tpl;
 
 import com.atomikos.icatch.jta.UserTransactionImp;
-import guru.qa.niffler.data.Databases;
 import jakarta.transaction.SystemException;
 import jakarta.transaction.UserTransaction;
 
-import java.sql.Connection;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 
