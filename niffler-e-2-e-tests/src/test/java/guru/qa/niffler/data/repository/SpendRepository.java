be82ba557879bac74f20459d8eefab59ef7d5d1b
sasoriq <vikotoed@gmail.com>
2026-06-29 16:32:22 +0700

    add spendRepository

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/repository/SpendRepository.java
@@ -0,0 +1,36 @@
+package guru.qa.niffler.data.repository;
+
+import guru.qa.niffler.data.entity.spend.CategoryEntity;
+import guru.qa.niffler.data.entity.spend.SpendEntity;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SpendRepository {
+    SpendEntity create(SpendEntity spend);
+
+    SpendEntity update(SpendEntity spend);
+
+    Optional<SpendEntity> findById(UUID id);
+
+    List<SpendEntity> findByUsername(String username);
+
+    List<SpendEntity> findAll();
+
+    void delete(SpendEntity spend);
+
+    CategoryEntity createCategory(CategoryEntity category);
+
+    Optional<CategoryEntity> findCategoryById(UUID id);
+
+    Optional<CategoryEntity> findCategoryByUsernameAndSpendName(String username, String name);
+
+    List<CategoryEntity> findCategoryByUsername(String username);
+
+    List<CategoryEntity> findAllCategories();
+
+    CategoryEntity updateCategory(CategoryEntity category);
+
+    void deleteCategory(CategoryEntity category);
+}
